7b48674986c8988f989e227de3daef19206ad157
TISparta <jose.garcia@utec.edu.pe>
2019-03-28 20:34:51 -0500

    Solucion del problema 3n + 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int compute (int n) {
+  int cnt = 1;
+  while (n != 1) {
+    cnt++;
+    if (n % 2 == 1) n = 3 * n + 1;
+    else n = n / 2;
+  }
+  return cnt;
+}
+
+int main () {
+  int l, r;
+  while (cin >> l >> r) {
+    int mx = 0;
+    for (int i = min(l, r); i <= max(l, r); i++) mx = max(mx, compute(i));
+    cout << l << ' ' << r << ' ' << mx << '\n';
+  }
+  return (0);
 }
